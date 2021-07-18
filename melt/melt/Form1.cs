cfad5c83b6c5fd1df012e18ae668e76c557c31b5
THEDERP655129 <83438557+THEDERP655129@users.noreply.github.com>
2021-07-18 10:07:32 -0700

    Add files via upload

---
+++ melt/melt/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace melt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                //TopMost = true;
+                int srwidth = Screen.PrimaryScreen.Bounds.Width;
+                int srheight = Screen.PrimaryScreen.Bounds.Height;
+
+                int rw = rnd.Next(1, srwidth + 1);
+            int r = rnd.Next(1, 20);
+
+                this.Height = srheight;
+                this.Location = new Point(rw, 0);
+
+
+                Bitmap ps = new Bitmap(srwidth, srheight);
+                Graphics g = Graphics.FromImage(ps);
+                g.CopyFromScreen(rw, 0, 0, r, ps.Size);
+                pictureBox1.Image = ps;
+                Width = 10;
+                Opacity = 100;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form2 f1 = new Form2();
+            f1.ShowDialog();
+        }
+    }
+}
---
+++ melt/melt/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace melt
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //TopMost = true;
+            int srwidth = Screen.PrimaryScreen.Bounds.Width;
+            int srheight = Screen.PrimaryScreen.Bounds.Height;
+
+            int rw = rnd.Next(1, srwidth + 1);
+            int r = rnd.Next(1, 20);
+
+            this.Height = srheight;
+            this.Location = new Point(rw, 0);
+
+
+            Bitmap ps = new Bitmap(srwidth, srheight);
+            Graphics g = Graphics.FromImage(ps);
+            g.CopyFromScreen(rw, 0, 0, r, ps.Size);
+            pictureBox1.Image = ps;
+            Width = 10;
+        }
+    }
+}
